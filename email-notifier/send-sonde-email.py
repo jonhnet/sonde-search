47e1c50ca1df8ebb18d76bced2ed1164fb270440
Jeremy Elson <jelson@gmail.com>
2023-01-30 15:31:25 -0800

    Refactor to send multiple emails

--- email-notifier/send-sonde-email.py
+++ email-notifier/send-sonde-email.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
 
+CONFIGS = [
+
+    # Seattle
+    {
+        'home_lat': 47.6426,
+        'home_lon': -122.32271,
+        'email_from': 'Seattle Sonde Notifier <jelson@lectrobox.com>',
+        'email_to': [
+            'jelson@gmail.com',
+            'jonh.sondenotify@jonh.net',
+        ],
+        'max_distance_miles': 200,
+    },
+
+    # Oakland
+    {
+        'home_lat': 37.859,
+        'home_lon': -122.270,
+        'email_from': 'Berkeley Sonde Notifier <jelson@lectrobox.com>',
+        'email_to': [
+            'jelson@gmail.com',
+            'jonh.sondenotify@jonh.net',
+            'david.jacobowitz+sonde@gmail.com',
+        ],
+        'max_distance_miles': 75,
+    },
+]
+
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -11,6 +39,7 @@ import base64
 import boto3
 import contextily as cx
 import datetime
+import geocoder
 import io
 import json
 import matplotlib.pyplot as plt
@@ -22,31 +51,11 @@ import sys
 
 cx.set_cache_dir(os.path.expanduser("~/.cache/geotiles"))
 
-LAT_MIN = 45
-LAT_MAX = 49
-LON_MIN = -125
-LON_MAX = -121
-
-HOME_LAT = 47.6485
-HOME_LON = -122.3502
-
-METERS_PER_MILE = 1609.34
-
 AWS_PROFILE = 'cambot-emailer'
-FROM_ADDR = 'Seattle Sonde Notifier <jelson@lectrobox.com>'
-
-TO_ADDRS = [
-    'jelson@gmail.com',
-    'jonh.sondenotify@jonh.net',
-]
-
-SONDEHUB_DATA_URL = 'https://api.v2.sondehub.org/sondes/telemetry?duration=6h'
+METERS_PER_MILE = 1609.34
+SONDEHUB_DATA_URL = 'https://api.v2.sondehub.org/sondes/telemetry?duration=12h'
 SONDEHUB_MAP_URL = 'https://sondehub.org/#!mt=Mapnik&mz=9&qm=12h&f={serial}&q={serial}'
 GMAP_URL = 'https://www.google.com/maps/search/?api=1&query={lat},{lon}'
-
-wgs84_to_mercator = Transformer.from_crs(crs_from='EPSG:4326', crs_to='EPSG:3857')
-def to_mercator_xy(lat, lon):
-    return wgs84_to_mercator.transform(lat, lon)
 
 # This is the old-style version that uses the sondehub python API, which goes to
 # sondehub's public S3 bucket for data. However, it's a few hours behind, and
@@ -68,37 +77,67 @@ def get_sonde_telemetry_api():
                 yield record
     return pd.DataFrame(unpack_list())
 
-def get_latest_sonde():
-    #df = get_sonde_telemetry_s3()
-    df = get_sonde_telemetry_api()
+def get_all_sondes():
+    #sondes = get_sonde_telemetry_s3()
+    sondes = get_sonde_telemetry_api()
 
     # Sometimes lat/lon comes as a string instead of float
-    df.lat = df.lat.astype(float)
-    df.lon = df.lon.astype(float)
+    sondes = sondes.astype({
+        'lat': float,
+        'lon': float,
+    })
 
-    # find only sondes in the home area
-    local = df.loc[
-        (df.lat >= LAT_MIN) &
-        (df.lat <= LAT_MAX) &
-        (df.lon >= LON_MIN) &
-        (df.lon <= LON_MAX)
-    ]
+    sondes['datetime'] = pd.to_datetime(sondes['datetime'])
 
-    if args.debug:
-        print(local)
+    # Mark takeoffs and landings -- the earliest ascent record and latest
+    # descent record for each serial number
+    ascents = sondes.loc[(sondes.vel_v > 0) & (sondes.alt < 15000)]
+    takeoffs = ascents.groupby('serial')['datetime'].idxmin()
+    sondes.loc[takeoffs, 'phase'] = 'takeoff'
 
-    # Find only descents:
-    landings = local.loc[
-        (local.vel_v < 0) &
-        (local.alt < 10000)
-    ]
+    descents = sondes.loc[(sondes.vel_v < 0) & (sondes.alt < 15000)]
+    landings = descents.groupby('serial')['datetime'].idxmax()
+    sondes.loc[landings, 'phase'] = 'landing'
 
-    if args.debug:
-        print(landings)
+    return sondes
 
-    # Sort by date and return just the latest landing
-    return landings.sort_values(by='datetime').iloc[-1]
 
+### Getting the nearest sonde
+
+def get_path(sonde, config):
+    path = Geodesic.WGS84.Inverse(config['home_lat'], config['home_lon'], sonde.lat, sonde.lon)
+    return {
+        'dist_from_home_mi': round(path['s12'] / METERS_PER_MILE),
+        'bearing_from_home': (round(path['azi1']) + 360) % 360,
+    }
+
+def get_nearest_sonde_flight(sondes, config):
+    # Annotate all sondes with distance from home
+    sondes = pd.concat([
+        sondes,
+        pd.DataFrame.from_records(sondes.apply(get_path, config=config, axis=1))
+    ], axis=1)
+
+    #f = sondes.sort_values('dist_from_home_mi')
+    #print(f[f.phase == 'landing'].to_string())
+
+    # Find the landing closest to home
+    nearest_landing_idx = sondes[sondes.phase == 'landing'].dist_from_home_mi.idxmin()
+
+    # Get the serial number of the nearest landing
+    nearest_landing_serial = sondes.loc[nearest_landing_idx].serial
+
+    # Return all data from the flight with the minimum landing distance
+    nearest_landing_flight = sondes.loc[sondes.serial == nearest_landing_serial]
+
+    return nearest_landing_flight
+
+
+#### Drawing map
+
+wgs84_to_mercator = Transformer.from_crs(crs_from='EPSG:4326', crs_to='EPSG:3857')
+def to_mercator_xy(lat, lon):
+    return wgs84_to_mercator.transform(lat, lon)
 
 MAP_WHITESPACE = 0.5
 
@@ -112,15 +151,15 @@ def get_limit(points):
     return min_x, min_y, max_x, max_y
 
 
-def get_image(args, landing):
-    home_x, home_y = to_mercator_xy(HOME_LAT, HOME_LON)
+def get_image(args, config, landing):
+    home_x, home_y = to_mercator_xy(config['home_lat'], config['home_lon'])
     sonde_x, sonde_y = to_mercator_xy(landing['lat'], landing['lon'])
 
     fig, ax = plt.subplots(figsize=(12, 12))
     ax.axis('off')
     ax.plot([home_x, sonde_x], [home_y, sonde_y], marker='*')
     min_x, min_y, max_x, max_y = get_limit([
-        [HOME_LAT, HOME_LON],
+        [config['home_lat'], config['home_lon']],
         [landing['lat'], landing['lon']],
     ])
     ax.set_xlim([min_x, max_x])
@@ -133,33 +172,51 @@ def get_image(args, landing):
     )
     fig.tight_layout()
 
-    if args.debug:
+    if not args.really_send:
         fig.savefig("test.jpg", bbox_inches='tight')
+
     return fig
 
 
-def main(args):
-    landing = get_latest_sonde()
-    path = Geodesic.WGS84.Inverse(HOME_LAT, HOME_LON, landing.lat, landing.lon)
-    if args.debug:
-        print(path)
+### Sending email
 
-    landing_time = pd.to_datetime(landing['datetime']).tz_convert('US/Pacific')
-    dist = round(path['s12'] / METERS_PER_MILE)
-    bearing = (round(path['azi1']) + 360) % 360
+def process(args, sondes, config):
+    flight = get_nearest_sonde_flight(sondes, config)
+    landing = flight.loc[flight.phase == 'landing'].iloc[0]
+
+    # attempt a geocode
+    geo = geocoder.osm(f"{landing.lat}, {landing.lon}")
+
+    place = ""
+    if geo and geo.county:
+        if geo.town:
+            place += geo.town + ", "
+        if geo.city:
+            place += geo.city + ", "
+        place += geo.county
+
+    # can be part of config eventually
+    landing_localtime = landing.datetime.tz_convert('US/Pacific')
 
     # subject line
-    subj = f"{landing_time.month_name()} {landing_time.day} "
-    subj += "morning" if landing_time.hour < 12 else "afternoon"
-    subj += f" sonde landed {dist}mi from home, bearing {bearing}°"
+    subj = f"{landing_localtime.month_name()} {landing_localtime.day} "
+    subj += "morning" if landing_localtime.hour < 12 else "afternoon"
+    subj += f" sonde landed {landing.dist_from_home_mi}mi from home,"
+    subj += f" bearing {landing.bearing_from_home}°"
+    if place:
+        subj += f" ({place})"
 
     # body
     body = f'Sonde <a href="{SONDEHUB_MAP_URL.format(serial=landing.serial)}">{landing.serial}</a> '
-    body += f'was last heard from at {landing_time.strftime("%Y-%m-%d %H:%M:%S")} Pacific time. '
+    body += f'was last heard at {landing_localtime.strftime("%Y-%m-%d %H:%M:%S")} Pacific time. '
     body += f'It landed near <a href="{GMAP_URL.format(lat=landing.lat, lon=landing.lon)}">{landing.lat}, {landing.lon}</a>, '
-    body += f'which is about {dist} miles from home at a bearing of {bearing}°.'
+    body += f'which is about {landing.dist_from_home_mi} miles from home at a bearing of {landing.bearing_from_home}°.'
+    if place:
+        body += f' It landed in {place}.'
+    if geo and geo.address:
+        body += f' The nearest known address is {geo.address}'
 
-    if args.debug:
+    if not args.really_send:
         print(subj)
         print(body)
 
@@ -169,14 +226,14 @@ def main(args):
     # build mime message including map attachment
     msg = MIMEMultipart('mixed')
     msg['Subject'] = subj
-    msg['From'] = FROM_ADDR
-    msg['To'] = ",".join(TO_ADDRS)
+    msg['From'] = config['email_from']
+    msg['To'] = ",".join(config['email_to'])
     alternatives = MIMEMultipart('alternative')
     alternatives.attach(MIMEText(body, 'html', 'utf-8'))
     msg.attach(alternatives)
 
     img = io.BytesIO()
-    fig = get_image(args, landing)
+    fig = get_image(args, config, landing)
     fig.savefig(img, format='jpg', bbox_inches='tight')
     img.seek(0)
     img_att = MIMEImage(img.read(), name='map.jpg')
@@ -186,10 +243,10 @@ def main(args):
     session = boto3.Session(profile_name=AWS_PROFILE)
     client = session.client('ses', region_name = 'us-west-2')
 
-    if not args.debug:
+    if args.really_send:
         client.send_raw_email(
-            Source=FROM_ADDR,
-            Destinations=TO_ADDRS,
+            Source=config['email_from'],
+            Destinations=config['email_to'],
             RawMessage={
                 'Data': msg.as_string(),
             },
@@ -198,12 +255,19 @@ def main(args):
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-d', '--debug',
+        '--really-send',
         default=False,
         action='store_true',
     )
     return parser.parse_args(sys.argv[1:])
 
-if __name__ == "__main__":
+def main():
     args = get_args()
-    main(args)
+
+    sondes = get_all_sondes()
+
+    for config in CONFIGS:
+        process(args, sondes, config)
+
+if __name__ == "__main__":
+    main()

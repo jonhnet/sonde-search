0c1921235b239d83bb32b82dad58a7615e5afef4
Jeremy Elson <jelson@gmail.com>
2024-02-02 01:05:07 -0800

    improve subscriber reporter

--- website/backend/src/show_subs.py
+++ website/backend/src/show_subs.py
@@ -4,6 +4,7 @@
 
 import sys
 import os
+import pandas as pd
 
 sys.path.insert(0, os.path.dirname(__file__))
 import table_definitions
@@ -18,15 +19,17 @@ def show_subs():
     users = users.sort_values('email_lc')
 
     subs = util.dynamodb_to_dataframe(t.subscriptions.scan)
-    subs = subs.sort_values('active')
+    subs['subscribe_time'] = pd.to_datetime(subs['subscribe_time'].fillna(0).astype(float), unit='s')
 
     for _, user in users.iterrows():
-        print(f"{user['uuid']:.8s} {user['email']}")
-
-        for _, sub in subs.loc[subs['subscriber'] == user['uuid']].iterrows():
+        print(f"{user['uuid']} {user['email']}")
+        for _, sub in subs.loc[subs['subscriber'] == user['uuid']].sort_values('subscribe_time').iterrows():
             s = '     '
-            s += '* ' if sub['active'] else '  '
-            s += f"{sub['max_distance_mi']:.1f}"
+            s += str(sub['subscribe_time'].floor('s'))
+            s += ' * ' if sub['active'] else '   '
+            s += f"{sub['max_distance_mi']:5.1f}"
+            s += f"{sub['lat']:8.3f}"
+            s += f"{sub['lon']:9.3f}"
             print(s)
 
         print()

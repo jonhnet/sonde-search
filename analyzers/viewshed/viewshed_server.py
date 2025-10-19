2f6d219201fceb69b0269b19489c6ca3bf29c275
Jon Howell <jonh@jonh.net>
2025-10-18 21:08:54 -0700

    relative paths

--- analyzers/viewshed/viewshed_server.py
+++ analyzers/viewshed/viewshed_server.py
@@ -62,7 +62,7 @@ class ViewshedServer:
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-    <link rel="stylesheet" href="/static/sidebar.css" />
+    <link rel="stylesheet" href="static/sidebar.css" />
     <style>
         * {
             box-sizing: border-box;
@@ -787,7 +787,7 @@ class ViewshedServer:
             });
         }
     </script>
-    <script src="/static/sidebar.js"></script>
+    <script src="static/sidebar.js"></script>
 </body>
 </html>
         """
